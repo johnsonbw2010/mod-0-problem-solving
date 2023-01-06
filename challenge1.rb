38824ad312fd66195e3f5b2af7293c70e98ad844
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-01-06 13:04:26 -0500

    Add pseudocode and states goal to challenge1

--- challenge1.rb
+++ challenge1.rb
@@ -1,8 +1,13 @@
 # Start with an array of strings. Print only the words that include
 # the letter combination "ing".
+###
+#The goal is to take an array of strings and pull only the ones that contain "ing".
+#The first thing to do is to create the array
+#We then need to write code that will analyze each string within the array
+#Once they're analyzed, we want to print only strings (word) if the string includes "ing".
 ###################
 
-strings = ["running", "jump", "sitting", "coding", "learn"]
+strings = ["running", "jump", "sitting", "coding", "learn","this is", "a collection", "of strings"]
 strings.each do |word|
   puts word if word.include?("ing")
 end
