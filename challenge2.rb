124924ec24e5221c1038a8ee08a8900f55b460f0
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-01-06 13:23:29 -0500

    Add challenge2

---
+++ challenge2.rb
@@ -0,0 +1,11 @@
+# Given an array of strings, return only the words that begin with the letter "t".
+###
+# The first step is to create a variable that is an array of strings.
+# The next step would be to write code that will scan each string and label them independently as "word"
+# Next, we check if the word starts with t - if true, then puts word.
+
+challenge2 = ["thanks","tutor","xylophone","truth","verified","hurricane"]
+challenge2.each do |word|
+    if word.start_with?"t" then puts word
+    end
+end
